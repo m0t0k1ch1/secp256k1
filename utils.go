e9b0cb8ecdd76e5034f9dab328e124a7b4e7de59
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-10-19 22:24:08 +0900

    rename func : fromHexToBigInt -> hexToBigInt

--- utils.go
+++ utils.go
@@ -2,7 +2,7 @@ package secp256k1
 
 import "math/big"
 
-func fromHexToBigInt(s string) *big.Int {
+func hexToBigInt(s string) *big.Int {
 	i, ok := new(big.Int).SetString(s, 16)
 	if !ok {
 		panic("invalid hex : " + s)
